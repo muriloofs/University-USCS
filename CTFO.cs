6ca01bd7c235c088d1ba85f605468fd1577f62f0
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-07 20:38:16 -0300

    more codes

---
+++ CTFO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTOF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double gf, gc;
+            Console.WriteLine("Digite o valor em graus Celsius: ");
+            gc = double.Parse(Console.ReadLine());
+            gf = (9 * gc + 160)/5;
+            Console.WriteLine("O valor em graus Fahrenheit é: " + gf);
+            Console.ReadKey();
+        }
+    }
+}
