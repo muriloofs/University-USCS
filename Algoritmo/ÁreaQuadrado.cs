779a9c02cd9ebcf78c517acf7be3ab438c7a9a98
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-09 18:46:50 -0300

    Create ÁreaQuadrado.cs

---
+++ Algoritmo/ÁreaQuadrado.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Área_Quadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, mult;
+            n1 = double.Parse(Console.ReadLine());
+            mult = n1 * n1;
+            Console.WriteLine(mult);
+        }
+    }
+}
