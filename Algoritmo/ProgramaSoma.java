60651615dd651cf1e13644324bb43d19d3f022e4
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-09 18:42:36 -0300

    Create ProgramaSoma.java

---
+++ Algoritmo/ProgramaSoma.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class ProgramaSoma {
+
+    public static void main(String[] args) {
+    //Fazer programa para somar numeros entre 1 e 10.
+        int n1 = 1, n2 = 10, soma = 0;
+        Scanner num = new Scanner(System.in);
+        while(n1 >= n2){
+            soma++;
+        }
+    }
+    
+}
