2358aa8becb48b2d5ef575e57f69ac03619efea3
Murilo <90356450+muriloofs@users.noreply.github.com>
2022-11-09 18:39:14 -0300

    Update and rename teste to 0Between10.cs

---
+++ Algoritmo/0Between10.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0Between10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( int i = 0; i <= 10; i++)
+            {
+                    Console.WriteLine("Numeros: " + i);
+            }
+        }
+    }
+}
